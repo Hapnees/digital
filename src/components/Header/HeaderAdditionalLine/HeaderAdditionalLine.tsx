41998f5a8a3826857c277257a25b0ab8e6764f3e
Hapnees <8dwardm8lton@gmail.com>
2023-05-09 12:54:54 +0300

    Feat (root): new styles for selected nav

--- src/components/Header/HeaderAdditionalLine/HeaderAdditionalLine.tsx
+++ src/components/Header/HeaderAdditionalLine/HeaderAdditionalLine.tsx
@@ -1,14 +1,32 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import cl from './HeaderAdditionalLine.module.scss'
+import parentCl from '../Header.module.scss'
+
+const navs = [
+  { title: 'Интернет-маркетинг', href: 'internetMarketing' },
+  { title: 'Киберспорт', href: 'cybersport' },
+  { title: 'Digital-агенства', href: 'digital' },
+  { title: 'Исследования', href: 'researches' },
+]
 
 const HeaderAdditionalLine = () => {
+  const { pathname } = useLocation()
+  console.log(pathname)
+
   return (
     <section className={cl.wrapper}>
       <nav className={cl.nav}>
-        <Link to='/category/internetMarketing'>Интернет-маркетинг</Link>
-        <Link to='/category/cybersport'>Киберспорт</Link>
-        <Link to='/category/digital'>Digital-агенства</Link>
-        <Link to='/category/researches'>Исследования</Link>
+        {navs.map(nav => (
+          <Link
+            key={nav.title}
+            to={`/category/${nav.href}`}
+            className={
+              `/category/${nav.href}` === pathname ? parentCl.tab_active : ''
+            }
+          >
+            {nav.title}
+          </Link>
+        ))}
       </nav>
     </section>
   )
--- src/components/Header/HeaderMainLine/HeaderMainLine.tsx
+++ src/components/Header/HeaderMainLine/HeaderMainLine.tsx
@@ -6,71 +6,72 @@ import LoginButton from '@/components/UI/LoginButton/LoginButton'
 import { useState } from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import cl from './HeaderMainLine.module.scss'
+import parentCl from '../Header.module.scss'
 
 type ITitleNav = 'Блоги' | 'Спец. проекты' | 'Лекториум' | 'Компании' | 'Законы'
 
 interface INav {
-	title: ITitleNav
-	href: string
+  title: ITitleNav
+  href: string
 }
 
 const HeaderMainLine = () => {
-	const navigte = useNavigate()
-	const [isShowModal, setIsShowModal] = useState(false)
+  const navigte = useNavigate()
+  const [isShowModal, setIsShowModal] = useState(false)
 
-	const { pathname } = useLocation()
+  const { pathname } = useLocation()
 
-	const onClickLogo = () => navigte('/')
+  const onClickLogo = () => navigte('/')
 
-	const onClickCross = () => setIsShowModal(true)
-	const onClickClose = () => setIsShowModal(false)
+  const onClickCross = () => setIsShowModal(true)
+  const onClickClose = () => setIsShowModal(false)
 
-	const navs: INav[] = [
-		{ title: 'Блоги', href: '/blogs' },
-		{ title: 'Спец. проекты', href: '/projects' },
-		{ title: 'Лекториум', href: '/lectorium' },
-		{ title: 'Компании', href: '/companies' },
-		{ title: 'Законы', href: '/category/laws' },
-	]
+  const navs: INav[] = [
+    { title: 'Блоги', href: '/blogs' },
+    { title: 'Спец. проекты', href: '/projects' },
+    { title: 'Лекториум', href: '/lectorium' },
+    { title: 'Компании', href: '/companies' },
+    { title: 'Законы', href: '/category/laws' },
+  ]
 
-	return (
-		<section className={cl.wrapper}>
-			<article className={cl.container}>
-				<Logo onClick={onClickLogo} />
+  return (
+    <section className={cl.wrapper}>
+      <article className={cl.container}>
+        <Logo onClick={onClickLogo} />
 
-				{/*CONTROLS*/}
-				<article className={cl.controls}>
-					{/*NAVBAR*/}
-					<nav className={cl.navbar}>
-						{navs.map(nav => (
-							<Link
-								key={nav.title}
-								to={nav.href}
-								className={
-									nav.href.includes(pathname) && pathname !== '/'
-										? cl.activeNav
-										: ''
-								}
-							>
-								{nav.title}
-							</Link>
-						))}
-					</nav>
+        {/*CONTROLS*/}
+        <article className={cl.controls}>
+          {/*NAVBAR*/}
+          <nav className={cl.navbar}>
+            {navs.map(nav => (
+              <Link
+                key={nav.title}
+                to={nav.href}
+                className={
+                  nav.href === pathname && pathname !== '/'
+                    ? parentCl.tab_active
+                    : ''
+                }
+              >
+                {nav.title}
+              </Link>
+            ))}
+          </nav>
 
-					<div className={cl.cross} onClick={onClickCross}></div>
+          <div className={cl.cross} onClick={onClickCross}></div>
 
-					<Input placeholder='Поиск...' />
+          <Input placeholder='Поиск...' />
 
-					<LoginButton>Войти</LoginButton>
-				</article>
-			</article>
-			{isShowModal && (
-				<Modal close={onClickClose}>
-					<PlusWindow />
-				</Modal>
-			)}
-		</section>
-	)
+          <LoginButton>Войти</LoginButton>
+        </article>
+      </article>
+      {isShowModal && (
+        <Modal close={onClickClose}>
+          <PlusWindow />
+        </Modal>
+      )}
+    </section>
+  )
 }
 
 export default HeaderMainLine
