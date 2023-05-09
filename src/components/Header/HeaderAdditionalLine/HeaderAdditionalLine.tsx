57563765921daa28762f7a08b0e6671ed05a8867
Hapnees <8dwardm8lton@gmail.com>
2023-05-09 12:44:00 +0300

    Feat (HeaderAdditionalLine): add navigation

--- src/components/Header/HeaderAdditionalLine/HeaderAdditionalLine.tsx
+++ src/components/Header/HeaderAdditionalLine/HeaderAdditionalLine.tsx
@@ -2,16 +2,16 @@ import { Link } from 'react-router-dom'
 import cl from './HeaderAdditionalLine.module.scss'
 
 const HeaderAdditionalLine = () => {
-	return (
-		<section className={cl.wrapper}>
-			<nav className={cl.nav}>
-				<Link to='/'>Интернет-маркетинг</Link>
-				<Link to='/'>Киберспорт</Link>
-				<Link to='/'>Digital-агенства</Link>
-				<Link to='/'>Исследования</Link>
-			</nav>
-		</section>
-	)
+  return (
+    <section className={cl.wrapper}>
+      <nav className={cl.nav}>
+        <Link to='/category/internetMarketing'>Интернет-маркетинг</Link>
+        <Link to='/category/cybersport'>Киберспорт</Link>
+        <Link to='/category/digital'>Digital-агенства</Link>
+        <Link to='/category/researches'>Исследования</Link>
+      </nav>
+    </section>
+  )
 }
 
 export default HeaderAdditionalLine
