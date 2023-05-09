c407ec6dccd7c20f4df24c39549d35cabadb36da
Hapnees <8dwardm8lton@gmail.com>
2023-05-09 12:43:46 +0300

    Feat (Board): add navigation

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,30 +1,37 @@
 import { IPost } from '@/types/post.interface'
 import { FC } from 'react'
 import cl from './Board.module.scss'
+import { useNavigate } from 'react-router'
 
 interface IProps {
-	titleBoard: string
-	posts: (IPost & Partial<{ category: string }>)[]
+  titleBoard: string
+  posts: IPost[]
 }
 
 const Board: FC<IProps> = ({ titleBoard, posts }) => {
-	return (
-		<article className={cl.wrapper}>
-			<h1 className={cl.title}>{titleBoard}</h1>
-			<ul className={cl.news}>
-				{posts.map(post => (
-					<li key={post.id}>
-						<div className={cl.header}>
-							{post.category && <p className={cl.category}>#{post.category}</p>}
-							<p className={cl.date}>{post.date}</p>
-						</div>
+  const navigate = useNavigate()
 
-						<p className={cl.newsTitle}>{post.title}</p>
-					</li>
-				))}
-			</ul>
-		</article>
-	)
+  const onClickNews = (post: IPost) => () => {
+    navigate(`/category/${post.category}/${post.id}`)
+  }
+
+  return (
+    <article className={cl.wrapper}>
+      <h1 className={cl.title}>{titleBoard}</h1>
+      <ul className={cl.news}>
+        {posts.map(post => (
+          <li key={post.id} onClick={onClickNews(post)}>
+            <div className={cl.header}>
+              {post.category && <p className={cl.category}>#{post.category}</p>}
+              <p className={cl.date}>{post.date}</p>
+            </div>
+
+            <p className={cl.newsTitle}>{post.title}</p>
+          </li>
+        ))}
+      </ul>
+    </article>
+  )
 }
 
 export default Board
