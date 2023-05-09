d4127a3cb074dcfda8dcae5443c0cd48b9eda27c
Hapnees <8dwardm8lton@gmail.com>
2023-05-09 23:37:46 +0300

    Fix (root): little fixes

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,17 +2,17 @@ import { DetailedHTMLProps, FC, HTMLAttributes } from 'react'
 import cl from './Logo.module.scss'
 
 interface IProps
-	extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-	newsColor?: string
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  newsColor?: string
 }
 
 const Logo: FC<IProps> = ({ newsColor = '#eee3e3', className, ...props }) => {
-	return (
-		<div className={`${cl.wrapper} ${className}`} {...props}>
-			<p>Диджитал</p>
-			<p style={{ color: newsColor }}>NEWS</p>
-		</div>
-	)
+  return (
+    <div className={`${cl.wrapper} ${className}`} {...props}>
+      <p>Диджитал</p>
+      <p>NEWS</p>
+    </div>
+  )
 }
 
 export default Logo
